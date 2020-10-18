dd41778ac261b51ebcbfe41615b853c8b8e1d7eb
haxorsprogramming <haxorsuinsu@gmail.com>
2020-10-19 02:49:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import React, { Component } from 'react';
+import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {name: 'Ninja Ken'};
-  }
-  
-  handleClick(name){
-    this.setState({name:name})
-  }
-  
-  render() {
+class App extends Component{
+
+  render(){
     return (
-    	<div>
-    	  <h1>Hello, {this.state.name}!</h1>
-        <Button variant="contained" color="primary" onClick={ () => {this.handleClick('Aditia Darma Nst')} }>
-          Ganti ke adit
-        </Button>
-        <BottomNavigation>
-        <BottomNavigationAction label="Recents" icon={<AccessAlarmIcon />} onClick={ () => {this.handleClick('Aditia Darma Nst')} }/>
-        </BottomNavigation>
+      <div className="App">
+        <h1>Aplikasi Kami</h1>
       </div>
-      
-    );
+    )
   }
+
 }
 
 export default App;
